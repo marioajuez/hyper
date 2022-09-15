40ae5d1945a4980a7fba194866411c502a95af7b
marioajuez <marioajuez@gmail.com>
2022-09-15 01:25:16 -0500

    same advances enable membership

--- src/app/services/membership/membership.service.ts
+++ src/app/services/membership/membership.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { from, Observable, of } from 'rxjs';
+import { from, Observable, of, Subscriber, Subscription, TeardownLogic } from 'rxjs';
 import { finalize, map } from 'rxjs/operators';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentData, Query, QueryFn, QueryGroupFn, Reference } from '@angular/fire/firestore';
 import { Hyperfund } from 'src/app/models/membership.model';
+
 // import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 
 @Injectable({
@@ -22,57 +23,73 @@ export class MembershipService {
     private firestore: AngularFirestore
     ) {
 
+      of(this.firestore.collection("memberships").ref.where('state', '==', true))
+      .pipe( 
+        map((query:Query)=> query),
+        map((query:Query)=> {
+
+          query.onSnapshot({
+            next: (snapshot) => {
+              snapshot.docChanges()
+
+            },
+            complete: () => {
+
+            }
+          })
+
+
+      
+          return query;
+
+
+
+        }
+
+
+        // let data = []
+        // return query.onSnapshot( snapshot => {
+
+        //   return snapshot.docChanges();
+        //     // snapshot.docChanges().forEach( change  => {
+        //     //     // console.log(change);
+        //     //     // data.push(change.doc.data());
+        //     // })
+
+        // })
+        
+
+      )).subscribe( resp =>{
+
+        console.log(resp);
+      })
+
+      // const prueba = this.firestore.collection("memberships").ref.where('state', '==', true) as Query;
+      // prueba.onSnapshot({
+
+      // })
+      // prueba.
+      // prueba.onSnapshot((e)=>{
+      //   console.log(e);
+
+      // }
+
+
+ 
+
       // this.firestore.collection("memberships").ref.orderBy('date','desc').onSnapshot( snapshot =>{
-      //   // console.log(snapshot);
-      //   snapshot.docChanges().forEach( change  => {
-      //     console.log(change.doc.data());
-      //   })
+
+  
+      //   // snapshot.docChanges().forEach( change  => {
+      //   //   console.log(change.doc.data());
+      //   // })
       // })
       //https://googleapis.dev/nodejs/firestore/latest/DocumentReference.html#update
   }
 
-  public createMemberShipsHyperfund(form): void {
-    const { 
-      initialMembershipLeverage,
-      minimumBalanceRebuy,
-      name,
-      percentRewards,
-      totalDays} = form;
-      
-    const membership = {
-        id: "2",
-        name: "membership 3.0",
-        totalDays: "1333",
-        initialMembershipLeverage: "4",
-        percentRewards: "0.003",
-        minimumBalanceRebuy: "125"
-    };
-    const memberships = JSON.parse(localStorage.getItem('memberships'));
-
-    console.log(memberships);
-    memberships.push(membership)
-    localStorage.setItem('memberships', JSON.stringify(memberships));
-  }
   
   public getMemberShipsHyperfund(): Observable<Hyperfund.Memberships> {
-    return this.httpClient.get('./assets/memberships.json').pipe(
-      map((resp: any) => {
-        const membershipsSaves = JSON.parse(localStorage.getItem('memberships')) as Array<any> || [];
-        const memberships = resp.memberships
-
-        let retornarMemberships = { memberships: []};
-        if (memberships.length > membershipsSaves.length){ 
-
-            retornarMemberships.memberships = memberships;
-            localStorage.setItem('memberships', JSON.stringify(memberships));
-        }else {
-            retornarMemberships.memberships = membershipsSaves;
-            localStorage.setItem('memberships', JSON.stringify(membershipsSaves));
-        }
-
-        return retornarMemberships;
-      })
-    ) as Observable<Hyperfund.Memberships>;
+    return this.httpClient.get('./assets/memberships.json').pipe() as Observable<Hyperfund.Memberships>;
   }
 
   public createMemberShipsFirebase( data: Hyperfund.Membership ): Observable<any>{
@@ -123,16 +140,9 @@ export class MembershipService {
 
   public updateStateMembership ( dataMembership: Hyperfund.Membership, idMembership: any ): Observable<any> {
     
-
-    const data: Hyperfund.Membership = {
-      name: dataMembership.name,
-      totalDays: dataMembership.totalDays,
-      initialMembershipLeverage: dataMembership.initialMembershipLeverage,
-      percentRewards: dataMembership.percentRewards,
-      decimalRewards: dataMembership.decimalRewards,
-      minimumBalanceRebuy: dataMembership.minimumBalanceRebuy,
+    const data: Hyperfund.Membership | any = {
       state:  dataMembership.state,
-      dateUpdate: dataMembership.dateUpdate
+      dateUpdate: dataMembership.dateUpdate,
     }
 
     return of(this.firestore.collection('memberships').doc(String(idMembership)).update(data)) as Observable<any>;
@@ -150,6 +160,24 @@ export class MembershipService {
     }
 
     return of(this.firestore.collection('memberships').doc(String(idMembership)).update(data)) as Observable<any>;
+  }
+
+  public getMemberShipsEnables() : Observable<any> {
+
+
+    // return of(this.firestore.collection("memberships").ref.where('state', '==', true).onSnapshot)
+
+
+    // return of(this.firestore.collection("memberships")
+    //   .ref.where('state', '==', true)
+    //   // .onSnapshot( snapshot =>{
+    //   //       snapshot.docChanges().forEach( change  => {
+    //   //       console.log(change.doc.data());
+    //   //   })
+    // }))
+
+
+    return new Observable();
   }
 
   get idAutoIncrementMembership(){
